77568d4fb351a00d949be65847e508c3c69ebc0d
reddibhavya <reddireddibhavya@gmail.com>
2025-09-28 13:00:17 +0800

    variables

--- common.sh
+++ common.sh
@@ -43,7 +43,15 @@ nodejs_setup(){
     VALIDATE $? "Install dependencies"
 }
 
-app_setup(){
+app_setup(){    
+    
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "Creating system user"
+    else
+        echo -e "User already exist ... $Y SKIPPING $N"
+    fi
     mkdir -p /app
     VALIDATE $? "Creating app directory"
 
