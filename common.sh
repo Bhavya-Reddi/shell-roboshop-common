81c50ab628265dc7f7b927034c82b5833079866d
reddibhavya <reddireddibhavya@gmail.com>
2025-09-29 18:07:11 +0800

    variables

--- common.sh
+++ common.sh
@@ -62,13 +62,20 @@ python_setup(){
 }
 
 app_setup(){
-    id roboshop &>>$LOG_FILE
-    if [ $? -ne 0 ]; then
-        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-        VALIDATE $? "Creating system user"
+    if ! id roboshop &>>$LOG_FILE; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
     else
-        echo -e "User already exist ... $Y SKIPPING $N"
+    echo -e "User already exists ... $Y SKIPPING $N" | tee -a $LOG_FILE
     fi
+
+    # id roboshop &>>$LOG_FILE
+    # if [ $? -ne 0 ]; then
+    #     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    #     VALIDATE $? "Creating system user"
+    # else
+    #     echo -e "User already exist ... $Y SKIPPING $N"
+    # fi
 
     mkdir -p /app
     VALIDATE $? "Creating app directory"
