c39a1f9af489e3ec164d288aa16aa0a9004ee9f0
reddibhavya <reddireddibhavya@gmail.com>
2025-09-29 17:12:15 +0800

    variables

--- common.sh
+++ common.sh
@@ -73,12 +73,12 @@ python_setup(){
 app_setup(){
     id roboshop &>>$LOG_FILE
     if [ $? -ne 0 ]; then
-        echo "➡️ roboshop user not found, creating..." | tee -a $LOG_FILE
+        # echo "➡️ roboshop user not found, creating..." | tee -a $LOG_FILE
         useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-        echo "✅ useradd command exited with status: $?" | tee -a $LOG_FILE
+        # echo "✅ useradd command exited with status: $?" | tee -a $LOG_FILE
         VALIDATE $? "Creating system user"
     else
-        echo -e "User already exist ... $Y SKIPPING $N" | tee -a $LOG_FILE
+        echo -e "User already exist ... $Y SKIPPING $N" # | tee -a $LOG_FILE
     fi
 
     
